48e6a072a2396fb5298fda10d04d96bee0d5f39d
Leo Li <327206596@qq.com>
2019-05-23 21:14:17 +1000

    establish routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const axios = require('axios');
+const routes = require('./routes');
 
 const app = express();
-
-app.get('/posts', (req, res) => {
-    res.send('welcome');
-});
-
-app.get('/api/weather/:cc/:city', (req, res) => {
-    // res.send('weather');
-    const {cc, city} = req.params;
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=285b431cbb23d069ca7a7be7dd48daef`)
-        .then(response => {
-            res.send(response.data);
-        }).catch(err => console.log(err));
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('app listen on port 3000'));
 
