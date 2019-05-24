d630181cc8f61784e306420f15d8620fb01f9ab9
Leo Li <327206596@qq.com>
2019-05-25 07:17:01 +1000

    add axios instance

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const axios = require('../utils/axios');
 
 const router = express.Router();
 const APPID = process.env.APPID;
@@ -7,7 +7,7 @@ const APPID = process.env.APPID;
 router.get('/:cc/:city', (req, res) => {
     // res.send('weather');
     const {cc, city} = req.params;
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=${APPID}`)
+    axios.get(`/weather?q=${city},${cc}`)
         .then(response => {
             res.send(response.data);
         }).catch(err => console.log(err));
